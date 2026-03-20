2b891a1cb70d1b26da1f480ebf760c76f8345474
sachinthapa <20028232@students.koi.edu.au>
2026-03-20 22:13:59 +1100

    chnage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,8 @@
-import Navbar from "../components/layout/navbar.jsx";
-import HeroSection from "../components/common/utility/heroSection.jsx";
-import Footer from "../components/layout/footer.jsx";
-import CategoriesSection from "../components/common/utility/categoriesSection.jsx";
-import FeaturedEventsSection from "../components/common/utility/featuresEvents.jsx";
-import AIRecommendationsSection from "../components/common/utility/aiRecomendationSection.jsx";
-import TestimonialsSection from "../components/common/utility/testomonialSection.jsx";
-import CTASection from "../components/common/utility/CTASection.jsx";
-import FeaturesSection from "../components/common/utility/featuresSection.jsx";
-
+import BrowseEvent from "../pages/BrowseEvent";
 function Index() {
   return (
     <>
+      <BrowseEvent />
     </>
   );
 }
