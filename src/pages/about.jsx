c81472604b7d645995f45aa59687d1bf5bf080bc
Manihang20031286 <20031286@students.koi.edu.au>
2026-03-27 17:22:25 +1100

    fix the asbout us section

---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+ import {Link} from 'react-router-dom';
+import Navbar from '../components/layout/Navbar';
+ function About(){
+    return (
+        <><Navbar/>
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to our about page!</p>
+        </div></>
+    );
+ }
+ export default About;
