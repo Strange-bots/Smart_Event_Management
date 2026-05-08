cf7fed83c3220600f00bfc37b2db1d48336b3fb5
sachinthapa <20028232@students.koi.edu.au>
2026-05-08 15:27:01 +1000

    checking issue

--- Server/src/app.js
+++ Server/src/app.js
@@ -8,6 +8,17 @@ const app = express();
 app.use(express.json({ limit: '6mb' }));
 app.use(corsMiddleware);
 app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')));
+app.get('/', (_req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    message: 'Smart Event Management backend is running',
+  });
+});
+app.get('/health', (_req, res) => {
+  res.status(200).json({
+    status: 'ok',
+  });
+});
 app.use('/api', apiRoutes);
 
 module.exports = app;
